93faf4a5c435880e2f8a0e8bea2e565d0ad5acb0
sparrow <2824111529@qq.com>
2020-11-30 15:54:19 +0800

    第一次修改

--- demo/src/main/java/test/demo/user/test.java
+++ demo/src/main/java/test/demo/user/test.java
@@ -1,8 +1,9 @@
 package test.demo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
+    public static void main(String[] args) {
 
+        System.out.println("第一次更改");
+    }
 }
